b2aa74c27c36b4cea0018ec43421664e9777c783
Divya Modi -X (divmodi - ZENSAR TECHNOLOGIES INC at Cisco) <divmodi@cisco.com>
2017-08-04 13:07:38 -0700

    Added the Detail View

--- foodtruck/urls.py
+++ foodtruck/urls.py
@@ -3,11 +3,15 @@ from .views import (
     getLists, create, update, 
     TruckListView,
     CreateTruckView,
+    TruckDetailView,
+    TruckFormView,
 )
 
 urlpatterns = [
     url(r'^trucklist/', getLists),
     # url(r'^?P<pk>[^0-9]+$', update),
     url(r'^create/', CreateTruckView.as_view(), name='truck-create'),
+    url(r'^form/', TruckFormView.as_view()),
     url(r'^list/', TruckListView.as_view(), name='truck-list'),
+    url(r'^details/(?P<pk>[0-9]+)/$', TruckDetailView.as_view(), name='truck-detials'),
 ]
