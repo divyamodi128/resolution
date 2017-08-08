c87631f560ba76964b2d90aa5649898158bfdcf4
Divya Modi -X (divmodi - ZENSAR TECHNOLOGIES INC at Cisco) <divmodi@cisco.com>
2017-08-08 11:44:01 -0700

    Added Image Upload for Trucks

--- foodtruck/forms.py
+++ foodtruck/forms.py
@@ -9,6 +9,16 @@ class TruckForm(forms.ModelForm):
             'description': forms.Textarea(attrs={'cols': 65, 'rows': 6}),
             'user': forms.HiddenInput()
         }
+    
+    def __init__(self, *args, **kwargs):
+        super(TruckForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            help_text = self.fields[field].help_text
+            self.fields[field].help_text = None
+            if help_text != '':
+                # Assigns the Help Text as a PlaceHolder
+                self.fields[field].widget.attrs.update({'placeholder':help_text})
+
     # name = forms.CharField(max_length=150)
     # website = forms.CharField(widget=forms.URLInput, max_length=250, required=False)
     # contact_no = forms.CharField(max_length=15, required=False)
--- foodtruck/models.py
+++ foodtruck/models.py
@@ -3,12 +3,13 @@ from django.conf import settings
 
 # Create your models here.
 class Truck(models.Model):
-    name = models.CharField(max_length=250, blank=False, null=False, help_text="Give your Truck a Nice Name")
-    website = models.CharField(max_length=250, blank=True, null=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    contact_no = models.CharField(max_length=15, blank=True, null=True)
-    emails = models.CharField(max_length=100, blank=True, null=True)
-    description = models.TextField()
+    name =          models.CharField(max_length=250, blank=False, null=False, help_text="Name Your Truck")
+    website =       models.CharField(max_length=250, blank=True, null=True)
+    image =         models.ImageField(upload_to='Truck-Images', blank=True, null=True)
+    user =          models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    contact_no =    models.CharField(max_length=15, blank=True, null=True)
+    emails =        models.CharField(max_length=100, blank=True, null=True)
+    description =   models.TextField()
 
     def __str__(self):
         return self.name
