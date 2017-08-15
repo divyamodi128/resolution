52f72706bd0811292e25d344f4712fbc26cb756a
Divya Modi -X (divmodi - ZENSAR TECHNOLOGIES INC at Cisco) <divmodi@cisco.com>
2017-08-15 14:30:26 -0700

    Modifications 9

--- resolutions/settings/main_settings.py
+++ resolutions/settings/main_settings.py
@@ -138,3 +138,6 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
