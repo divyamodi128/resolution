b5cd83afd7a24e952454b9690ff618e6188d30f3
Divya Modi -X (divmodi - ZENSAR TECHNOLOGIES INC at Cisco) <divmodi@cisco.com>
2017-08-23 15:57:00 -0700

    Added AWS S3 Storages

--- resolutions/settings/main_settings.py
+++ resolutions/settings/main_settings.py
@@ -150,3 +150,11 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# AWS S3 BUCKET Credentials.
+AWS_QUERYSTRING_AUTH = False
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+MEDIA_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
